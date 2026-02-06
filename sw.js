1f01342b62b207e976c5676dcd781efa346902d4
net0008 <herdogmus@hotmail.com>
2026-02-06 22:57:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'pardus-rehber-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap'
+];
+
+// Yükleme Anı (Dosyaları hafızaya at)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Çalışma Anı (İnternet yoksa hafızadan getir)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
