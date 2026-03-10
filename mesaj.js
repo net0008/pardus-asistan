7c6d78d323bd9012a2cb90088ba0efeb4dc1d49d
net0008 <herdogmus@hotmail.com>
2026-03-10 16:02:50 +0300

    Update mesaj.js

--- mesaj.js
+++ mesaj.js
@@ -2,10 +2,10 @@
 
 const announcements = [
     "<strong>Hatırlatma:</strong> İzmir İl MEM takvimine göre Pardus ETAP 23 geçiş işlemleri 1 Nisan 2026'ya kadar tamamlanmalıdır.",
-    `<strong>İpucu:</strong> Arama kutusuna Windows'taki adını (Örn: Görev Yöneticisi) yazarak Pardus karşılığını bulabilirsiniz.`,
+    //`<strong>İpucu:</strong> Arama kutusuna Windows'taki adını (Örn: Görev Yöneticisi) yazarak Pardus karşılığını bulabilirsiniz.`,
     `<strong>Önemli:</strong> Tahtanız donduğunda fişi çekmeyin, güç düğmesine 10-15 saniye basılı tutarak güvenle kapatın.`,
-    `<strong>Biliyor muydunuz?</strong> Pardus Kalem'de arka planı şeffaf yaparak Z-Kitap üzerine çizim yapabilirsiniz.`,
-    `<strong>Güvenlik:</strong> Sınıftan çıkarken tahtanızı ETA USB Anahtarınız ile kilitlemeyi unutmayın.`
+    //`<strong>Biliyor muydunuz?</strong> Pardus Kalem'de arka planı şeffaf yaparak Z-Kitap üzerine çizim yapabilirsiniz.`,
+    //`<strong>Güvenlik:</strong> Sınıftan çıkarken tahtanızı ETA USB Anahtarınız ile kilitlemeyi unutmayın.`
 ];
 
 let currentAnnouncementIndex = 0;
